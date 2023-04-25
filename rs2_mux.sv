5a3acbd10879dca1a1007154f4f50ac58f18a4e6
avinash91231 <125534349+avinash91231@users.noreply.github.com>
2023-04-25 23:43:29 +0530

    Delete rs2_mux.sv

--- rs2_mux.sv
+++
@@ -1,8 +0,0 @@
-module rs2_mux16x1 (rs2, rs2_regflag, rs2_output);
-  
-  input [3:0] rs2;
-  input [15:0] rs2_regflag;
-  output reg [15:0] rs2_output;
-  
-  assign rs2_out = rs2_regflag[rs2];
-endmodule 
